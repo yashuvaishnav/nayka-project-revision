1c16401384c86f3384af356df8329fb1c4148d54
yashuvaishnav <yashurishu22@gmail.com>
2024-01-05 11:07:44 +0530

    Add changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require("express");
+require("dotenv").config();
 const app=express();
 const cors=require("cors");
 const { connection } = require("./db");
@@ -15,7 +16,7 @@ app.get("/",(req,res)=>{
     res.send("server is working");
 })
 
-app.listen(8080,async()=>{
+app.listen(process.env.port,async()=>{
     try{
         await connection;
         console.log("server is running at port 8080");
